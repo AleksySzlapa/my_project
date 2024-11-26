5e112ca19b6ff3b0c15cdd3db1b5165928ea2174
Aleksy Szlapa <u8_aleszl_lbn@technischools.com>
2024-11-26 10:46:49 +0000

    added save and get chat

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,16 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
+}
+
+
+#[ic_cdk::update]
+fn save_chat(input_chat: String) {
+    CHAT.with(|chat|chat.borrow_mut().push(input_chat))
+}
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn get_chat()  -> Vec<String> {
+    CHAT.with(|chat| chat.borrow().clone())
 }
