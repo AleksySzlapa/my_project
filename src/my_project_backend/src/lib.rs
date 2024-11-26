6150e5885a4d58472a48f639b8429824dd78b434
Aleksy Szlapa <u8_aleszl_lbn@technischools.com>
2024-11-26 12:36:22 +0000

    added front, removal of last mess

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -10,6 +10,14 @@ fn save_chat(input_chat: String) {
     CHAT.with(|chat|chat.borrow_mut().push(input_chat))
 }
 
+#[ic_cdk::update]
+fn del_last_chat() {
+    CHAT.with(|chat| {
+        let mut chat_vec = chat.borrow_mut();
+        chat_vec.pop(); // Removes the last element, if it exists
+    });
+}
+
 #[ic_cdk::query]
 fn get_chat()  -> Vec<String> {
     CHAT.with(|chat| chat.borrow().clone())
--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,17 +1,24 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let greeting = ref('');
+let chat = ref('');
+
+async function getChats() {
+  chat.value = await my_project_backend.get_chat()
+}
 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const chat = target.querySelector('#chat').value;
-  await my_project_backend.save_chat(chat).then((response) => {
-    greeting.value = response;
-  });
+  const msg = target.querySelector('#chat').value;
+  await my_project_backend.save_chat(msg)
+  await getChats()
 }
+
+
 </script>
+
+
 
 <template>
   <main>
@@ -23,6 +30,8 @@ async function handleSubmit(e) {
       <input id="chat" alt="Chat" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <section id="greeting">
+      <div v-for="msg in chat">{{ msg }}</div>
+    </section>
   </main>
 </template>
